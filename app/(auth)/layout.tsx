513f12e89604f1c78da241ef76b6d09437f9f7e0
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-18 19:57:21 +0600

    authentication added

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,6 @@
+const AuthLayout =({children}:{children: React.ReactNode})=>{ 
+    return (
+    <div className="flex items-center justify-center h-full bg-gradient-to-r from-teal-500 to-blue-950">{children} </div>
+    
+   )}
+   export default AuthLayout
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+ const SetupPage=()=> {
+    return (
+      
+      <div> This is a protected route
+        <Button size="default" variant="destructive" className="p-4">click</Button>
+      </div>
+    )
+  }
+  export default SetupPage
