064660cb3d9de5e301fdae519291e7e3b30afa16
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-28 05:13:50 +0600

    store-name update functionality added with api

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -8,9 +8,12 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Store } from "@prisma/client"
+import axios from "axios"
 import { Trash } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
 import * as z from "zod"
 type SettingsFormValues = z.infer<typeof formSchema>
 
@@ -20,7 +23,8 @@ interface SettingsFormProps{
 const formSchema = z.object({name: z.string().min(2),});
 export const SettingsForm : React.FC<SettingsFormProps>=({initialData})=>{
     
-    
+    const params=useParams()
+    const router=useRouter()
     const [open,setOpen]=useState(false)
 
     const [loading,setLoading]=useState(false)
@@ -32,7 +36,19 @@ export const SettingsForm : React.FC<SettingsFormProps>=({initialData})=>{
       });
 
     const onSubmit =async (data:SettingsFormValues) => {
-        console.log(data)
+        //console.log(data)
+        try{
+            setLoading(true)
+            await axios.patch(`/api/stores/${params.storeId}`,data)
+            router.refresh()
+            toast.success("Store Updated.")
+        }
+        catch(error){
+            toast.error("Something went wrong.")
+        }
+        finally{
+            setLoading(false)
+        }
     }
     return(
     <>    
