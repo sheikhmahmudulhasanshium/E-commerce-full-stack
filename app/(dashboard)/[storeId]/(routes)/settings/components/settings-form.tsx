c77f99b192fa183649bbc70c9297143ed8a6e06b
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-28 04:40:23 +0600

    settings-form added

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,69 @@
+//this form can be created anywhere but it will be used only in settings
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Heading } from "@/components/ui/heading"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Store } from "@prisma/client"
+import { Trash } from "lucide-react"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+type SettingsFormValues = z.infer<typeof formSchema>
+
+interface SettingsFormProps{
+    initialData: Store
+}
+const formSchema = z.object({name: z.string().min(2),});
+export const SettingsForm : React.FC<SettingsFormProps>=({initialData})=>{
+    
+    
+    const [open,setOpen]=useState(false)
+
+    const [loading,setLoading]=useState(false)
+
+    
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+      });
+
+    const onSubmit =async (data:SettingsFormValues) => {
+        console.log(data)
+    }
+    return(
+    <>    
+        <div className="flex items-center justify-between">
+            {/**This is reusable component */}
+            <Heading title="Settings" description="Manage Store Preferences"/>
+            <Button disabled={loading} variant="destructive" size="sm" onClick={()=>setOpen(true)}>
+                <Trash className="w-4 h-4"/>
+            </Button>
+        </div>
+        <Separator/>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
+                <div className="grid grid-cols-3 gap-8">
+                    <FormField 
+                        control={form.control} 
+                        name="name" 
+                        render={({field})=>(
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input disabled={loading} placeholder="Store name" {...field}/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}/>
+                </div>
+                <Button disabled={loading} className="ml-auto" type="submit">Save Changes</Button>
+            </form>
+        </Form>
+        
+    </>
+    )
+}
