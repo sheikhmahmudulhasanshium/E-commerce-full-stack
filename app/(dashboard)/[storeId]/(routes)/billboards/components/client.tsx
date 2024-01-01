789e399fec24ca5fa6a6d858c8d81cc096520cb6
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-01 23:18:29 +0600

    api list is added

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
 import { BillboardColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
 interface BillboardClientProps{
     data: BillboardColumn []
@@ -30,6 +31,9 @@ export const BillboardClient:React.FC<BillboardClientProps>=({data})=>{
             </div>
             <Separator/>
             <DataTable columns={columns} data={data} searchKey="label"/>
+            <Heading title="API" description="Api calls for Billboard"/>
+            <Separator/>
+            <ApiList entityName="billboards" entityIdName="billboardId"/>
         </>
     )
 }
