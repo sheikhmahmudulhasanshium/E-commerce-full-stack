5ee5c269e11e90f1fcefaf554e5525c7460916f4
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-06 00:29:33 +0600

    use origin updated

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useOrigin=()=>{
-    const [mounted,setMounted]=useState(false)
-    const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
+export const useOrigin = () => {
+  const [mounted, setMounted] = useState(false);
+  const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
     //hydration trick
-    useEffect(()=>{
-        setMounted(true)
-    },[])
-    if(!mounted){
-        return ''
-    }
-    return origin
-}
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return ''
+  }
+
+  return origin;
+};
