db72d54e4aaf16be1f39a272c210c127d3ea1d59
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-01-10 02:32:22 +0600

    graph functionality added with recharts

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,6 +1,8 @@
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
 import { getSalesCount } from "@/actions/get-sales-count";
 import { getStockCount } from "@/actions/get-stock-count";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { Overview } from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
@@ -12,10 +14,11 @@ interface DashboardPageProps{
     params:{storeId:string}
 }
 const DashboardPage:React.FC<DashboardPageProps> =async ({params}) => {
+    //fetching data from functions
     const totalRevenue=await getTotalRevenue(params.storeId)
     const salesCount=await getSalesCount(params.storeId)
     const stockCount=await getStockCount(params.storeId)
-
+    const graphRevenue=await getGraphRevenue(params.storeId)
 
     return ( 
         <div className="flex-col">
@@ -54,6 +57,14 @@ const DashboardPage:React.FC<DashboardPageProps> =async ({params}) => {
                             <div className="text-2xl font-bold">
                                 {stockCount}
                             </div>
+                        </CardContent>
+                    </Card>
+                    <Card className="col-span-4">
+                        <CardHeader>
+                            <CardTitle>Overview</CardTitle>
+                        </CardHeader>
+                        <CardContent className="pl-2">
+                            <Overview data={graphRevenue}/>
                         </CardContent>
                     </Card>
                 </div>
